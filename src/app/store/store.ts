41cf658e19bf3d12cf7d451e08adfade0d8923cc
Ricardo <40615609+rickyMelida@users.noreply.github.com>
2024-04-16 21:54:39 -0400

    added project

---
+++ src/app/store/store.ts
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+import taskReducer from "../../features/tasks/taskSlice";
+
+export const store = configureStore({
+    tasks: taskReducer
+});
---
+++ src/features/tasks/taskSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const taskSlice = createSlice({
+  name: "task",
+  initialState: [],
+  reducers: {},
+});
+
+export default taskSlice.reducer;
